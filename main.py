f05a04bdfdba6a17755dfa82580a7720e0f07d10
willy kristianto <willykristianto89@gmail.com>
2020-05-11 23:19:10 +0700

    add jaeger

--- main.py
+++ main.py
@@ -61,9 +61,9 @@ def initialize_tracer():
     return jaeger_config.initialize_tracer()
 
 if __name__ == '__main__':
-    # tracer = initialize_tracer()
-    # sanic_tracing = SanicTracing(tracer, trace_all_requests=True, app=app)
+    tracer = initialize_tracer()
+    sanic_tracing = SanicTracing(tracer, trace_all_requests=True, app=app)
     app.config.from_object(ConfigEnv)
     app.blueprint(bp_expeditions_v1)
-    # setup_middlewares(app)
-    app.run(host=app.config.HOST, port=app.config.PORT, debug=True, auto_reload=True, workers=2)
+    setup_middlewares(app)
+    app.run(host=app.config.HOST, port=app.config.PORT, debug=app.config.DEBUG, auto_reload=app.config.DEBUG, workers=2)
