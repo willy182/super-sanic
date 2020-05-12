43a3f77edac3a35647f869243916c87842d27412
willy kristianto <willykristianto89@gmail.com>
2020-05-12 11:42:04 +0700

    unset total data expedition

--- src/v1/expeditions/usecase/usecase_expeditions.py
+++ src/v1/expeditions/usecase/usecase_expeditions.py
@@ -17,13 +17,12 @@ class ListExpeditionUsecase(uc.UseCase):
     async def process_request(self, request_objects):
         try:
             data_expedition = await asyncio.create_task(self.repo.expedition.get_all(request_objects))
-            data_total_expedition = await asyncio.create_task(self.repo.expedition.get_total(request_objects))
             data_area = await asyncio.create_task(self.repo.area.get_subdistrict_by_zipcode(17111))
-            data_plankton = await asyncio.create_task(
-                self.repo.plankton.get_variant('/variants?filter[skuNo]=3316920142')
-            )
+            data_plankton = await asyncio.create_task(self.repo.plankton.get_variant('/variants?filter[skuNo]=3316920142'))
 
-            total_page = ceil(data_total_expedition / request_objects.limit)
+            # await data_expedition
+            # await data_area
+            # await data_plankton
 
             serializer = ExpeditionBaseSchema().dump(data_expedition, many=True)
 
@@ -31,20 +30,7 @@ class ListExpeditionUsecase(uc.UseCase):
                 data['subDistrict'] = data_area
                 data['plankton'] = data_plankton
 
-            response = {
-                'success': True,
-                'code': Config.STATUS_CODES[Config.SUCCESS],
-                'message': Config.SUCCESS.lower(),
-                'meta': {
-                    'page': request_objects.page,
-                    'limit': request_objects.limit,
-                    'totalRecords': data_total_expedition,
-                    'totalPages': total_page,
-                },
-                'data': serializer.data
-            }
-
-            return ro.ResponseSuccess(response)
+            return ro.ResponseSuccess(serializer)
 
         except Exception as e:
             return CommonResponse.build_common_message(str(e), Config.SYSTEM_ERROR)
