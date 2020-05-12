068fcafbd3ddf54ddff299fbffcf8b96d893704f
willy kristianto <willykristianto89@gmail.com>
2020-05-12 11:38:27 +0700

    unassigned db read

--- src/v1/expeditions/repository/repository_postgres.py
+++ src/v1/expeditions/repository/repository_postgres.py
@@ -12,7 +12,7 @@ class ExpeditionsRepositoryPSQL(ExpeditionsRepository):
     async def get_all(self, filters):
         query = select([expeditions]).limit(10).offset(0)
         try:
-            data = await self.db['read'].fetch_all(query)
+            data = await self.db.fetch_all(query)
         except Exception as e:
             data = e
 
@@ -21,7 +21,7 @@ class ExpeditionsRepositoryPSQL(ExpeditionsRepository):
     async def get_total(self, filters):
         query = select([func.count(expeditions.c.id)])
         try:
-            data = await self.db['read'].fetch_all(query)
+            data = await self.db.fetch_all(query)
         except Exception as e:
             data = e
 
@@ -30,7 +30,7 @@ class ExpeditionsRepositoryPSQL(ExpeditionsRepository):
     async def get_by_id(self, id):
         query = expeditions.select().where('id' == id)
         try:
-            data = await self.db['read'].fetch_one(query)
+            data = await self.db.fetch_one(query)
         except Exception as e:
             data = e
 
